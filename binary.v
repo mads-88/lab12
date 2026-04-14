50f8a6df67aa91212ac69cbb0f089670bf38b358
mads-88 <madisonhickey@u.boisestate.edu>
2026-04-14 15:24:08 -0600

    Update binary.v

--- binary.v
+++ binary.v
@@ -1,1 +1,44 @@
 // Implement binary state machine
+
+module binary(
+    input w,
+    input clk,
+    input reset,
+    output z,
+    output [2:0] State
+);
+
+    wire [2:0] Next;
+
+    dff two(
+        .Default(1'b0),
+        .D(Next[2]),
+        .clk(clk),
+        .reset(reset),
+        .Q(State[2])
+    );
+
+    dff one(
+        .Default(1'b0),
+        .D(Next[1]),
+        .clk(clk),
+        .reset(reset),
+        .Q(State[1])
+    );
+
+    dff zero(
+        .Default(1'b0),
+        .D(Next[0]),
+        .clk(clk),
+        .reset(reset),
+        .Q(State[0])
+    );
+
+    assign z = State[2] | (State[1] & ~State[0]);
+
+    assign Next[2] = w & (State[2] | (State[1] & State[0]));
+    assign Next[1] = (State[0] & ~State[1]) | (State[1] & ~State[0]) | (w & ~State[0] & ~State[2]);
+    assign Next[0] = (State[1] & State[0] & ~w) | (w & ~State[0] & ~State[2]) |
+                     (w & ~State[1] & ~State[2]) | (~State[0] & ~State[1] & ~w);
+
+endmodule
